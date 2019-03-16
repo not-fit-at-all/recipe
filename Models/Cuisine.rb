387ee8c606a8f4bab4aabb160db738c8f9085ca0
not-fit-at-all <48248513+not-fit-at-all@users.noreply.github.com>
2019-03-16 17:55:14 +0900

    cuisine bugfix

--- Models/Cuisine.rb
+++ Models/Cuisine.rb
@@ -20,71 +20,70 @@ class Cuisine
       return true
     else
       return self.missing_ingredients
-  end
+    end
 
-  def list_missing_ingredients(available_ingredients)
-    @missing_ingredients = Array.new
-    number_of_ai = available_ingredients.length
-    @required_ingredients.each do |ri|
-      quantity = 0
-      miss = 0
-      available_ingredients.each do |ai|
-        if @name == ai.name
-          quantity += ai.quantity
+    def list_missing_ingredients(available_ingredients)
+      @missing_ingredients = Array.new
+      number_of_ai = available_ingredients.length
+      @required_ingredients.each do |ri|
+        quantity = 0
+        miss = 0
+        available_ingredients.each do |ai|
+          if @name == ai.name
+            quantity += ai.quantity
+          end
+        end#ai
+        if quantity == 0 #placeholder. should compared to required quantity,saved in cuisine list
+          @missing_ingredients << ri
         end
-      end#ai
-      if quantity == 0 #placeholder. should compared to required quantity,saved in cuisine list
-        @missing_ingredients << ri
-      end
-    end#ri
-    return @missing_ingredients
-  end
+      end#ri
+      return @missing_ingredients
+    end
 
 
-  class << self
-    def list(known_ingredients = KnownIngredient.list, ai)
-      puts "Cuisine.list"
-      cuisines_list = Array.new #contains all the cuisines on the file
-      required_ingredients = Array. new #temporarily holds the required ingredients
-      length = known_ingredients.length
-      puts "  the number of known ingredients = #{length}"
-      file = File.open("assets/list_of_cuisines.txt") #list_of_cuisines.txt
-      cuisine_name = String.new #temporarily stores the name of the cuisine
-      required_ingredients = Array.new #temporarily stores th required ingredients
-      file.each_line do |line|
-        if line =~ /^#/ #cuisine's name
-          cuisine_name = line.chomp.delete("#") #temporarily holds the names of the cuisine
-
-        elsif line =~ /^$/ #end of the required ingredients
-          cuisines_list << Cuisine.new(cuisine_name, required_ingredients, ai)
-          cuisine = nil
-          required_ingredients =Array.new
-          #puts "END"
-          #puts ""
-          next
-
-        else
-          missed = 0
-          known_ingredients.each do |ki|
-            if line.chomp == ki.name
-              required_ingredients << ki
-            else
-              missed += 1
-              if missed == length
-                puts "  can't find #{line.chomp}"
-                #later it shoud ask to add the ki.
-                #(need the method to add things to assets)
-              end
-            end#if
+    class << self
+      def list(known_ingredients = KnownIngredient.list, ai)
+        puts "Cuisine.list"
+        cuisines_list = Array.new
+        required_ingredients = Array. new #temporarily holds ri
+        length = known_ingredients.length
+        #puts "  the number of known ingredients = #{length}"
+        file = File.open("assets/list_of_cuisines.txt")
+        cuisine_name = String.new #resets
+        required_ingredients = Array.new #temporarily stores th required ingredients
+        file.each_line do |line|
+          if line =~ /^#/ #cuisine's name
+            cuisine_name = line.chomp.delete("#") #temporarily holds the names of the cuisine
+          elsif line =~ /^$/ #end of the required ingredients
+            cuisines_list << Cuisine.new(cuisine_name, required_ingredients, ai)
+            cuisine = nil
+            required_ingredients =Array.new
+            #puts "END"
+            #puts ""
+            next
+          else
+            missed = 0
+            known_ingredients.each do |ki|
+              if line.chomp == ki.name
+                required_ingredients << ki
+              else
+                missed += 1
+                if missed == length
+                  puts "  can't find #{line.chomp}"
+                  #later it shoud ask to add the ki.
+                  #(need the method to add things to assets)
+                end
+              end#if
+            end
           end#if line
           #need to test this method
           #View.show_list(required_ingredients)
         end
       end
       return cuisines_list
-    end
-  end
-end
+    end #method
+  end #class methods
+end #class
 =begin
 to find missing ingredients, for each required_ingredients I need to
 look up on an array that is the product of Controller.list_available_ingredients
